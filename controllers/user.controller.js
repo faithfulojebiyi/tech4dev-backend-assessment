501f7aad2037ee05c9cad42145ed746dfe2391d8
faithfulojebiyi <faithfulojebiyi@gmail.com>
2021-05-03 22:38:12 +0100

    Update Cursor limit to 20

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,7 +3,7 @@ const { userService } = require("../services");
 
 const getUsers = catchAsync(async (req, res) => {
   const pageNumber = req.query.page;
-  const pageSize = 2;
+  const pageSize = 20;
   const skip = (pageNumber - 1) * pageSize;
   const limit = pageSize;
   const user = await userService.getAllUsers(skip, limit);
